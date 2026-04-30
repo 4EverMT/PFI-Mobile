6a598a85a94955db52b2464af5802f06924657f4
Mathis Teixeira <mathis.teixeira44@gmail.com>
2026-04-30 16:51:19 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import{Link } from 'expo-router'
 export default function Page() {
   return (
+<<<<<<< Updated upstream
     <View>
       <View>
         <Text>MAIN PAGE</Text>
@@ -10,3 +11,38 @@ export default function Page() {
     </View>
   );
 }
+=======
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.title}>MAIN PAGE</Text>
+        <Link href="/(tabs)" style={styles.text}> CONNECTER </Link>
+        </ImageBackground>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    color: "black",
+    fontSize: 42,
+    textAlign: "center",
+    backgroundColor: "white"
+  },
+    text: {
+    color: "black",
+    fontSize: 24,
+    textAlign: "center",
+    backgroundColor: "white"
+  },
+});
+
+export default Page
+>>>>>>> Stashed changes
